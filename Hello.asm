5118559cbb9328e97bebf78f3d8a5280575e87e6
MKFMIKU <mikumkf@outlook.com>
2016-06-06 18:33:21 +0800

    Push HelloWorld

---
+++ Hello.asm
@@ -0,0 +1,16 @@
+data segment
+    msg db 'Hello World','$'
+data ends
+
+code segment
+    assume cs:code,ds:data
+    start:
+            mov ax,data
+            mov ds,ax
+            lea dx,msg
+            mov ah,9h
+            int 21h
+            mov ah,4ch
+            int 21h
+    code ends
+    end start
